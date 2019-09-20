2c95fef72c5192a8b1083626398294cc37b6a08b
dandewaters <dandewaters@gmail.com>
2019-09-20 16:26:45 -0400

    more plot 6

--- scripts/plot6.R
+++ scripts/plot6.R
@@ -2,29 +2,27 @@
 SCC_motor <- SCC$SCC[grep("Motor", SCC$Short.Name)]
 
 # Get Baltimore motor vehicle observations
-baltimore_LA_NEI <- NEI[NEI$fips == "24510"|NEI$fips == "06037",]
+baltimore_LA_NEI <- NEI[NEI$fips=="24510" | NEI$fips=="06037",]
 motor_NEI <- baltimore_NEI[baltimore_LA_NEI$SCC %in% SCC_motor,]
 
 # Group motor vehicle emissions by year and area
 motor_groups <-
   motor_NEI %>%
   group_by(year, fips) %>%
-  summarise(total_emissions = sum(Emissions), mean_emissions=mean(Emissions))
+  summarise(total_emissions=sum(Emissions), mean_emissions=mean(Emissions))
 
 # Make PNG file
 png("./plots/plot6.png", width=720, height=480)
 
 # Makes plot frames
 with(motor_groups,
-     plot(total_emissions ~ year,
-          type="n",
-          xlab="Year", ylab="Total Emissions"))
+     plot(total_emissions~year, type="n", xlab="Year", ylab="Total Emissions", col=motor_groups$fips))
 
 # Add main title
 mtext("Total Motor Vehicle Emissions in Baltimore and Los Angeles from 1999 to 2008", outer=TRUE)
 
 # Add data to plot
-lines(motor_groups$year, motor_groups$total_emissions, col=fips)
+lines(motor_groups$year, motor_groups$total_emissions)
 
 # Close png file
 dev.off()
